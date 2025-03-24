04ad3ce8380f3c782fe22f1f9aee36f94cc8143d
Yulia Gurevich <yulia.v.gurevich@gmail.com>
2025-03-24 16:33:46 +0200

    Save

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import path from 'path';
-// import { errors } from 'celebrate';
 import cors from 'cors';
 
 import cookieParser from 'cookie-parser';
--- backend/src/config.ts
+++ backend/src/config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export const port = process.env.PORT || 3001;
 export const corsOrigin = process.env.ORIGIN_ALLOW || 'http://localhost:5173';
-export const dbAddress = process.env.DB_ADDRESS || '';
+export const dbAddress = process.env.DB_ADDRESS || 'mongodb://127.0.0.1:27017/weblarek';
 export const accessTokenSecretKey = process.env.ACCESS_TOKEN_SECRET_KEY || 'access_token_secret_key';
 export const refreshTokenSecretKey = process.env.REFRESH_TOKEN_SECRET_KEY || 'refresh_token_secret_key';
 export const accessTokenLifetime = process.env.AUTH_ACCESS_TOKEN_EXPIRY || '1m';
